ce669f8bfbbeb90930b4edcee14e8c2e1614be84
Miloszekilola2009 <spoconysedes15@gmail.com>
2025-09-12 09:43:33 +0200

    menupt2

--- Assets/Scripts/butonswork.cs
+++ Assets/Scripts/butonswork.cs
@@ -2,8 +2,20 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class butonswork : MonoBehaviour
 {
-    public void LoadScene(string scene1 )
+    public void LoadScene1()
     {
         SceneManager.LoadScene("scene1");
     }
+
+    public void LoadSaves()
+    {
+        SceneManager.LoadScene("saves");
+    }
+
+    public void Quitgame()
+    {
+        Application.Quit();
+    }
+    
+        
 }
