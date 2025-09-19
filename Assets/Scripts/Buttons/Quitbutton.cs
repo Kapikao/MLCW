dd019c5c945f82e06ba8b9c5ec049f492e911361
Zofia <spoconysedes15@gmail.com>
2025-09-19 10:19:07 +0200

    przyciski nier dokończne
    trzeba zrobic aby działało z najechaniem myszka

---
+++ Assets/Scripts/Buttons/Quitbutton.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Quitbutton : MonoBehaviour
+{
+    SpriteRenderer spriteRenderer;
+    public Sprite normalSprite;    // Domyœlny sprite
+    public Sprite hoverSprite;     // Sprite po najechaniu myszk¹
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+        // Ustaw domyœlny sprite na start
+        if (normalSprite != null)
+            spriteRenderer.sprite = normalSprite;
+    }
+
+    void OnMouseEnter()
+    {
+        if (spriteRenderer != null && hoverSprite != null)
+            spriteRenderer.sprite = hoverSprite;
+    }
+
+    void OnMouseExit()
+    {
+        if (spriteRenderer != null && normalSprite != null)
+            spriteRenderer.sprite = normalSprite;
+    }
+
+}
